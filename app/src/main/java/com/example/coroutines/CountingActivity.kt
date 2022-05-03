bff615bef08779cd36bdabc2a7c71698ebe9d079
PawanRoy1997 <pawanroy0066@gmail.com>
2022-05-03 14:39:38 +0530

    code Cleanup

--- app/src/main/java/com/example/coroutines/CountingActivity.kt
+++ app/src/main/java/com/example/coroutines/CountingActivity.kt
@@ -1,8 +1,10 @@
 package com.example.coroutines
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.coroutines.databinding.ActivityCountingBinding
 
@@ -12,19 +14,41 @@ class CountingActivity : BaseActivity<ActivityCountingBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this)[CountingViewModel::class.java]
-        view.startCounting.setOnClickListener(startCountingListener())
-        view.stopCounting.setOnClickListener(stopCountingListener())
-        viewModel.counterActive.observe(this){ active ->
+
+        view.apply {
+            startCounting.setOnClickListener(startCountingListener())
+            stopCounting.setOnClickListener(stopCountingListener())
+            dataStoreBtn.setOnClickListener(dataStoreBtnListener())
+        }
+
+        viewModel.apply {
+            counterActive.observe(this@CountingActivity, jobObserver())
+            count.observe(this@CountingActivity, countObserver())
+        }
+    }
+
+    private fun dataStoreBtnListener(): View.OnClickListener {
+        return View.OnClickListener {
+            viewModel.stopCounting()
+            startActivity(Intent(this, DataStoreActivity::class.java))
+        }
+    }
+
+    private fun countObserver(): Observer<Int> {
+        return Observer {
+            view.count.text = it.toString()
+            Log.d("Counting", it.toString())
+        }
+    }
+
+    private fun jobObserver(): Observer<Boolean> {
+        return Observer { active ->
             view.apply {
                 startCounting.isClickable = !active
                 stopCounting.isClickable = active
             }
             Log.d("Counting", "startCountBtn : ${!active}")
             Log.d("Counting", "stopCountBtn : $active")
-        }
-        viewModel.count.observe(this) {
-            view.count.text = it.toString()
-            Log.d("Counting", it.toString())
         }
     }
 
